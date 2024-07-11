b160b340c73cf23a0ee4daaa77bdf14a6d597da0
hayong39 <yia0524_hy@naver.com>
2024-07-11 12:51:45 +0900

    03-3 두 번째 리액트 기초 개념: JSX

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+
 ReactDOM.render(
     <App />, //App이라는 컴포넌트를 
     document.getElementById('root') //id가 root인 엘리먼트에 그리기
